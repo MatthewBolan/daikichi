3b139112ceea91c5772b48b1becc6b53992b092b
Matthew Bolan <Bolan.Matthew.MB@Gmail.com>
2021-10-01 18:54:51 -0500

    Path Variables Added

--- DaikichiApplication.java
+++ DaikichiApplication.java
@@ -3,6 +3,7 @@ package com.codingdojo.daikichi;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,5 +52,37 @@ public class DaikichiApplication {
             return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
             
     }
+    
+    
+    @RequestMapping("/daikichi/travel/{city}")
+    
+    // 3. Method that maps to the request route above
+    
+    public String showcity(@PathVariable ("city") String city) {
+    	
+            return "Congratulations! You will soon travel to " + city + "!";
+            
+    }
+    
+    @RequestMapping("/daikichi/lotto/{number}")
+    
+    // 3. Method that maps to the request route above
+    
+    public String showlottonumber(@PathVariable ("number") int number) {
+    	
+    	if (number % 2 == 0 ) {
+    		
+    		return "You will take a grand journey in the near future, but be weary of tempting offers.";
+    		
+    	}
+    	
+    	else {
+    		
+    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    		
+    	}
+                    
+    }
+    
     
 }
